d45e8e3a1241416bd786bdcbe92d41a22322323e
Tejashwini Rao <polepalle.07@gmail.com>
2023-06-18 16:27:42 +0530

    changes

--- createjobs/main.tf
+++ createjobs/main.tf
@@ -9,17 +9,17 @@ resource "jenkins_job" "infra-jobs" {
   count      = length(var.infra-jobs)
   name       = element(var.infra-jobs, count.index)
   folder     = "/job/infrastructure"
-  template = templatefile("${path.module}/pipeline-job.xml", {
+  template = templatefile("pipeline-job.xml", {
     git_repo = element(var.infra-jobs, count.index)
   })
 }
 
-resource "jenkins_job" "app-jobs" {
-  depends_on = [jenkins_folder.folder]
-  count      = length(var.app-jobs)
-  name       = element(var.app-jobs, count.index)
-  folder     = "/job/applications"
-  template = templatefile("${path.module}/multi-branch-job.xml", {
-    git_repo = element(var.app-jobs, count.index)
-  })
-}
+#resource "jenkins_job" "app-jobs" {
+#  depends_on = [jenkins_folder.folder]
+#  count      = length(var.app-jobs)
+#  name       = element(var.app-jobs, count.index)
+#  folder     = "/job/applications"
+#  template = templatefile("${path.module}/multi-branch-job.xml", {
+#    git_repo = element(var.app-jobs, count.index)
+#  })
+#}
